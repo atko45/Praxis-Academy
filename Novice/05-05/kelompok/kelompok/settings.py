0d4fad66963d282c38242ca1be1d25767745b8c3
unknown <okta.brahvila45@gmail.com>
2020-09-23 14:38:21 +0700

    Catatan Keuangan

--- Novice/05-05/kelompok/kelompok/settings.py
+++ Novice/05-05/kelompok/kelompok/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'kelompok.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'praxis_academy',
+        'USER': 'postgres',
+        'PASSWORD': 'Dantes45',
+        'HOST': 'localhost',
+        'PORT': '4500',
     }
 }
 
